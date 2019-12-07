19a15fcf5d186500a13dfc5add4bca191ee63b85
stefanbit1996 <changbit1996@gmail.com>
2019-12-07 10:18:16 +0800

    simple calculation

---
+++ 3-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("please enter the first value\n");
+	scanf("%d", &a);
+	printf("please enter the second value\n");
+	scanf("%d", &b);
+	c = a + b;
+	printf("the sum is %d\n", c);
+	return 0;
+}
